d7efc073605f592d61b8730201642260577859ce
bedlam343 <jagik22@gmail.com>
2025-01-18 15:31:43 -0800

    Track component

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,8 +1,8 @@
-import { Track } from 'src/utils/types';
-import Tooltip from 'src/components/common/Tooltip';
+import Track from 'src/components/Track';
+import { Track as TrackType } from 'src/utils/types';
 
 type TrackListProps = {
-  tracks: Track[];
+  tracks: TrackType[];
   onLikeTrack: (trackId: string) => void;
   onUnlikeTrack: (trackId: string) => void;
 };
@@ -28,65 +28,11 @@ const TrackList = ({ tracks, onLikeTrack, onUnlikeTrack }: TrackListProps) => {
 
       <div className="flex justify-center gap-[30px] max-w-[900px] flex-wrap">
         {tracks.map((track, index) => (
-          <div
-            key={track.id}
-            style={{
-              opacity: 0,
-              animationDelay: `${0.1 * index}s`,
-            }}
-            className="animate-appear flex flex-col items-center gap-[20px]"
-          >
-            <a
-              href={track.external_urls.spotify}
-              target="_blank"
-              className="flex flex-col items-center w-[150px] gap-[10px] group"
-            >
-              <div className="flex w-full justify-between">
-                <img
-                  className="w-[90px]"
-                  src="src/assets/spotify/full_logo_white.png"
-                />
-
-                <div className="flex justify-end items-end w-full invisible group-hover:visible">
-                  <img
-                    src="src/assets/arrow_outward.png"
-                    className="w-[20px] h-[20px]"
-                  />
-                </div>
-              </div>
-
-              <div className="w-[150px] h-[150px] flex justify-center items-center">
-                <img
-                  className="transition duration-300 tranform ease-in-out h-[150px] w-[150px] group-hover:scale-95"
-                  src={track.album.images[0].url}
-                />
-              </div>
-
-              <div>
-                <p className="text-center text-stone-300 font-bold group-hover:underline">
-                  {track.name}
-                </p>
-
-                <p className="text-stone-400 text-center">
-                  {track.artists[0].name}
-                </p>
-              </div>
-            </a>
-
-            <Tooltip text="Add to Liked Songs">
-              <img
-                onClick={() =>
-                  handleIconClick(track.id, Boolean(track.likedByCurrentUser))
-                }
-                className="w-[20px] h-[20px] hover:cursor-pointer"
-                src={`src/assets/spotify/${
-                  track.likedByCurrentUser
-                    ? 'like_icon_liked.png'
-                    : 'like_icon_like.png'
-                }`}
-              />
-            </Tooltip>
-          </div>
+          <Track
+            track={track}
+            animationDelay={0.1 * index}
+            onLikeIconClick={handleIconClick}
+          />
         ))}
       </div>
     </div>

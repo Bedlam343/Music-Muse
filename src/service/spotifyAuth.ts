c6b90e6dcf7806ce4cecd6d9f41ff7eab6d39a42
bedlam343 <jagik22@gmail.com>
2025-01-23 12:15:18 -0800

    encoded_spotify_client_secret env variable for development

--- src/service/spotifyAuth.ts
+++ src/service/spotifyAuth.ts
@@ -1,6 +1,6 @@
-const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID as string;
-const SPOTIFY_CLIENT_SECRET = import.meta.env
-  .VITE_SPOTIFY_CLIENT_SECRET as string;
+const SPOTIFY_ENCODED_CLIENT_SECRET = import.meta.env
+  .VITE_SPOTIFY_ENCODED_CLIENT_SECRET;
+const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 const MODE = import.meta.env.MODE;
 
 import {
@@ -131,9 +131,7 @@ export const fetchClientAccessToken = async () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
-          Authorization: `Basic ${btoa(
-            `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
-          )}`,
+          Authorization: `Basic ${SPOTIFY_ENCODED_CLIENT_SECRET}`,
         },
         body: 'grant_type=client_credentials',
       });
